c4bfa1591559ffa9d1be6d234efef38a9ce4ae28
anaismiraira <mirairaanais@gmail.com>
2021-11-04 22:52:23 -0400

    2252 04112021 trello2

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,12 +23,12 @@ $('.owl-carousel').owlCarousel({
     margin:0,
     nav:true,
 })
-$('.owl-table').owlCarousel({
-    loop:true,
-    items:1,
-    margin:0,
-    nav:true,
-})
+// $('.owl-table').owlCarousel({
+//     loop:true,
+//     items:1,
+//     margin:0,
+//     nav:true,
+// })
 
 
 if (screen.width < 992){
